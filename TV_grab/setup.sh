8a7c40ead97134cfcb70714aff4f3d80da82e003
neo365 <neo365@gmail.com>
2016-08-21 23:34:39 +0900

    Ver 0.1
    - 전체 코드 정리 및 주석추가
    - 설명 추가.

---
+++ TV_grab/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# tv_grab_file setup
+
+cp src/tv_grab_kr_btv /usr/bin
+cp src/tv_grab_kr_ko /usr/bin
+cp src/tv_grab_kr_olleh /usr/bin
+cp src/tv_grab_kr_tvG /usr/bin
+
+chmod +x  /usr/bin/tv_grab_kr_btv
+chmod +x  /usr/bin/tv_grab_kr_ko
+chmod +x  /usr/bin/tv_grab_kr_olleh
+chmod +x  /usr/bin/tv_grab_kr_tvG
